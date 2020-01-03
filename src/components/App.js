25bed6e6a3eaaf84e7aed30d3bcbb7b3bf1b218c
Roman <roman.dronov1@gmail.com>
2020-01-04 00:31:12 +0300

    Feature/support page (#2)
    * feature/support-page Added support-page function
    
    Support page functions are presented as alpha version 0.1. Donuts to everyone!
    
    * feature/support-page Added support-page function
    
    Deleted chinese symbols.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>Hello, this is Peter Parker.</div>
+            <div>I'm ready to support you my fellow friend.</div>
         );
     }
 }
